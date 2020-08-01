5060e3c5bc9aa58a433a039119d582865ab4a44d
Jesus Estrada <jesuestrada@gmail.com>
2020-08-01 10:33:07 -0500

    newTweet

--- src/components/Private/Tweets/index.js
+++ src/components/Private/Tweets/index.js
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import NewTweet from './../NewTweet';
 import Tweet from './../Tweet';
 
 const Tweets = () => {
     const [tweets, setTweets] = useState([]);
 
     const getTweets = () => {
+        const token = localStorage.getItem('token');
         const api = process.env.REACT_APP_API_URL;
         const url = `${api}/tweets`;
-        fetch(url)
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                "x-access-token":token
+            }
+        })
         .then(res=>res.json())
         .then(json=>{
             setTweets(json);
         })
+    }
+
+    const addTweet = (tweet) => {
+        setTweets([tweet, ...tweets]);
     }
 
     useEffect(()=>{
@@ -20,6 +31,7 @@ const Tweets = () => {
 
     return (
         <>
+            <NewTweet addTweet={addTweet} />
             <h2>Tweets</h2>
             {
                 tweets ? 
